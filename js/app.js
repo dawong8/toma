0ae29a68aea7adee16b3c8cd9ac5876bf897b1ba
Danny Wong <wong.danny29@gmail.com>
2019-01-04 08:36:09 -0800

    functions done

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 
+let lights = true; 
+let intervalID; 
 
 const pet = {
 
 	name: "", 
-	health: 100, 
-	hunger: 100, 
+	health: 50, 
+	hunger: 50, 
 	happiness: 100, 
-	type: ""
+	shower: 50,
+	age: 0,
+	type: "", 
+	baby: true
 
 }; 
 
-console.log(pet);
+
+//cache 
+
+const $pet = $("#mypet");
 
 
 // Event Listeners
@@ -33,23 +41,26 @@ $("#litwick,#cleffa,#togepi,#azurill").on("click", (e) => {
 
 		switch (pet.type) {
 			case "togepi": 
-				$("#mypet").attr("src", "images/togepi.gif");
+				$pet.attr("src", "images/togepi.gif");
 				break;
 			case "azurill": 
-				$("#mypet").attr({
+				$pet.attr({
 					src: "images/azurill.gif",
 					class: "pet"
 				});
 				break;
 			case "cleffa":
-				$("#mypet").attr("src", "images/cleffa.gif");
+				$pet.attr("src", "images/cleffa.gif");
 				break;
 			default: 
 				break;
 		}
 
+		render();
+
 	}
 })
+
 
 
 
@@ -86,5 +97,170 @@ $(".container").on("click", (e) => {
 
 
 });
+
+
+$("#press-shower").on("click", (e) => {
+	$(".showering").toggleClass("hide");
+
+
+	let temp = setTimeout(function() {
+		// console.log("running")
+		$(".showering").toggleClass("hide");
+		pet.shower += 10; 
+		$("#shower-bar").val(pet.shower);
+	}, 1000);
+	console.log(pet);
+
+});
+
+
+$("#press-feed").on("click", (e) => {
+
+	$(".feeding").toggleClass("hide");
+
+	let temp = setTimeout(function() {
+		// console.log("running")
+		$(".feeding").toggleClass("hide");
+		pet.hunger += 10; 
+		$("#hunger-bar").val(pet.hunger);
+	}, 1000);
+	console.log(pet);
+
+});
+
+
+
+
+$("#press-lights").on("click", function(e) {
+	if (lights) {
+		$("#playground").css({
+			filter: "brightness(10%)"
+		});
+
+
+		$(this).css({
+			background: "white"
+		});
+
+
+		lights = false;
+
+	} else {
+		$("#playground").css("filter", "brightness(100%)");
+		$(this).css({
+			background: "grey"
+		});
+		lights = true; 
+	}
+});
+
+
+$pet.on("click", function(e) {
+	if (pet.happiness < 255) {
+		pet.happiness+= 5;
+	}
+	$("#happiness-bar").val(pet.happiness);
+});
+
+
+// Functions 
+
+
+function changeState() {
+	pet.age++;
+	$("#age").text(`Age: ${pet.age} Days Old`);
+
+	changePet();
+
+
+	$("#hunger-bar").val(pet.hunger);
+	$("#shower-bar").val(pet.shower);
+
+
+	$("#health-bar").val(pet.health);
+
+	evolve();
+
+	if (petDead()) {
+		clearInterval(intervalID);
+
+		$(".active").css("display", "none");
+		$("#dead").toggleClass("hide");
+		$("#dead-screen").text(`${pet.name} survived for ${pet.age} days.`).css("font-size", "50px");
+
+
+		console.log("pet has died");
+	} 
+
+}
+
+
+
+
+function evolve() {
+	if (pet.happiness == 255 && pet.type != "litwick" && pet.baby == true) {
+		console.log("evolving");
+
+
+		switch (pet.type) {
+			case "cleffa": 
+				$pet.attr("src", "images/clefairy.gif").css("width", "15%");
+				break;
+			case "togepi":
+				$pet.attr("src", "images/togetic.gif").css("align-self", "center");
+				break;
+			case "azurill": 
+				$pet.attr("src", "images/marill.gif").css("width", "15%");
+				break;
+
+			default: 
+				break;
+
+		}
+
+		pet.baby = false;
+	}
+}
+
+
+function changePet() {
+	if (pet.hunger > 0) {
+		pet.hunger--; 
+	}
+	if (pet.shower > 0) {
+		pet.shower--;
+	}
+
+	if (pet.hunger == 0 || pet.shower == 0) {
+		pet.health -= 5; 
+	} else if (pet.hunger >= 50 && pet.shower >= 50) {
+		pet.health += 10; 
+	}
+}
+
+function petDead() {
+
+	if (pet.health == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+
+function render() {
+	intervalID = window.setInterval(changeState, 1500); // change later!!!! 1500
+
+
+}
+
+
+
+
+
+
+
+
 
 
